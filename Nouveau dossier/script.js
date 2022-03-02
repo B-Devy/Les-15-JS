d8cc4f84974c92b9447a7b32eb6172a2f0ffb4cd
B-Devy <bertrandcafolla@hotmail.com>
2022-03-02 18:23:28 +0100

    day 02

--- Nouveau dossier/script.js
+++ Nouveau dossier/script.js
@@ -1,35 +1,45 @@
-var color = ["red", "blue", "yellow", "green"];
+const color = ["red", "blue", "yellow", "green"];
+const tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 const button = document.querySelector('button');
-const button2 = document.getElementsByTagName('button')[1];
-
+var alerte = document.getElementById('alerte');
+var code = document.getElementById('code');
 var body = document.querySelector('body');
 body.style.backgroundColor = "red";
 
 let i = 0;
+var tog = "";
 
+function alarme() {
+    alerte.setAttribute('style','display:block;')
+    var okay = document.getElementById('okay');
+    okay.addEventListener('click', () => {alerte.setAttribute('style','display:none;')});  /*-----okay.style.display = "none"   marche pt aussi-----*/
+}
 /*------------------------*/
 button.addEventListener('click', () => {
-    i++;
-    if (i == color.length) {
-        i = 0;      
-    }  
-        body.style.transition = "all 0.3s";
-        body.style.backgroundColor = color[i];
-});
+    if (tog == "simple") {
+        i++;
+        if (i == color.length) {
+            i = 0;      
+        }  
+            body.style.transition = "all 0.3s";
+            body.style.backgroundColor = color[i];
+            code.textContent = " " + color[i];
 
-/*------------------------*/
-const tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    } else if (tog == "complexe") {
+        var couleur = "#"; 
 
-button2.addEventListener('click', () => {
-    var couleur = "#"; 
-
-    for (let x = 0; x < 6; x++) {
-        var num = Math.floor(Math.random() * 16);
-        couleur += tab[num];
+        for (let x = 0; x < 6; x++) {
+            var num = Math.floor(Math.random() * 16);
+            couleur += tab[num];
+            }
+            body.style.transition = "all 0.3s";
+            body.style.backgroundColor = couleur;
+            code.textContent = " " + couleur;
+   
+    } else {
+        alarme();
     }
     
-    body.style.transition = "all 0.3s";
-    body.style.backgroundColor = couleur;
 });
 
 /*------------------------*/
@@ -38,30 +48,20 @@ var menu2 = document.getElementsByClassName('menu')[1];
 var idmenu1 = document.getElementById('colsim');
 var idmenu2 = document.getElementById('colcom');
 
-var texto = document.getElementsByClassName('texte')[0];
-var tog = "simple";
-
-
 menu1.addEventListener('click', () => {
     tog = "simple";
-    texto.innerText = tog;
     
     idmenu1.classList.add('valide');
-    idmenu2.classList.remove('valide');
-    
+    idmenu2.classList.remove('valide');  
 });
 
 menu2.addEventListener('click', () => {
     tog = "complexe";
-    texto.innerText = tog;
     
     idmenu2.classList.add('valide');
-    idmenu1.classList.remove('valide');
-    
+    idmenu1.classList.remove('valide');   
 });
 
-console.log(idmenu1);
-console.log(idmenu2);
 
 
 
